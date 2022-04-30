01481d5bc9410eae12cc2c013d60c4a7f1b77ca5
Vusani Makhomu <makhomuvusani23@gmail.com>
2022-05-01 00:16:17 +0200

    Removed pdf state in the Form

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import { jsPDF } from "jspdf";
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       nameInputValue: "",
+      downloadPdf: false,
+      userNameValue: "",
+      averageBMIValue: 0,
+      percentageIncreaseValue: 0,
+      numParticipantsValue: 0,
     };
+    this.savePDFDetails = this.savePDFDetails.bind(this);
   }
 
   // On file select
@@ -15,17 +22,20 @@ class Form extends React.Component {
     // Get the selected file.
     const file = event.target.files[0];
     const fileReader = new FileReader();
+    var sumBMI = 0;
+    var array = [];
+    var overWeightPercentageIncrease = 0;
+    const scope = this
+    var onloader = function(){this.setState({ image: reader.result });
+    fileReader.onload = 
     fileReader.onload = function (event) {
     const text = event.target.result;
-    
-    // console.log("Here is the file text:", text)
-    // console.log("Here is your name:", name.text);
 
       const csvHeader = text.slice(0, text.indexOf("\n")).split(",");
       const csvRows = text.slice(text.indexOf("\n") + 1).split("\n");
 
       // Convert CSV file data to Array
-      const array = csvRows.map(i => {
+      array = csvRows.map(i => {
         const values = i.split(",");
         const obj = csvHeader.reduce((object, header, index) => {
           object[header] = values[index];
@@ -33,8 +43,6 @@ class Form extends React.Component {
         }, {});
         return obj;
       });
-
-      var sumBMI = 0;
 
       /*
         Calculate the sum bmi of the given data.
@@ -52,18 +60,40 @@ class Form extends React.Component {
         Percentage increase formula:
             ((final value - starting value) / starting value) * 100
       */
-      var overWeightPercentageIncrease = 0;
+      
       if (sumBMI > 25) {
           overWeightPercentageIncrease = ((sumBMI - 25)/25)*100;
       }
 
       sumBMI = Math.round(sumBMI);
       overWeightPercentageIncrease = Math.round(overWeightPercentageIncrease);
-      // console.log("Sum BMI:", sumBMI);
-      // console.log("Percentage increase:", overWeightPercentageIncrease);
-      // console.log("Here is the user name:", userName);
+
+      // Generate PDF
+      // var doc = new jsPDF();
+
+      // doc.setFontSize(40);
+      // doc.setFont("helvetica", "bold");
+      // doc.text("Our Statistics", 100, 25, null, null, "center");
+      // doc.setFontSize(20);
+      // doc.setFont("times", "italic");
+      // doc.text("Name: "+userName, 90, 35, null, null, "center");
+      // doc.text("Average BMI: "+sumBMI, 90, 45, null, null, "center");
+      // doc.text("% (Percentage) Overweight: "+overWeightPercentageIncrease, 85, 55, null, null, "center");
+      // doc.text("Number of participants: "+array.length, 85, 65, null, null, "center");
+      // doc.save("Example.pdf");
+      // console.log("Should be displaying the download PDF page now!");
+      // this.savePDFDetails(userName, sumBMI, overWeightPercentageIncrease, array.length);
+      // console.log("Just called the savePDFDetails function.");
+      // scope.setState({
+      //   // nameInputValue: enteredValue,
+      //   userNameValue: userName,
+      //   averageBMIValue: sumBMI,
+      //   percentageIncreaseValue: overWeightPercentageIncrease,
+      //   numParticipantsValue: array.length,
+      // });
   };
   fileReader.readAsText(file);
+
 };
 
 saveNameInputValue = event => {
@@ -73,39 +103,96 @@ saveNameInputValue = event => {
   });
 };
 
+savePDFDetails = () => {
+  console.log("Printing from the savePDFDetails function.");
+  console.log("Here is the participants name: "+this.state.userNameValue);
+  console.log("Here is the average bmi value: "+this.state.averageBMIValue);
+  // console.log("Here is the bmi: "+bmi);
+  // console.log("Here is the overweight percentage: "+percentage);
+  // console.log("Here is the number of participants: "+participants);
+};
+
   render() {
-    return (
-      <div>
-        <div class="formContainer">
+
+      if (!this.props.showPopUp) {
+        return (
           <div>
-          <h1>Welcome</h1>
-          <p>Please complete your details, and upload<br/> your data to the platform.</p>
-          <form id="landingPageForm">
-            <label for="name">What is your name?</label>
-            <input type="text" name="name" id="name" value={this.state.nameInputValue} onChange={event => this.saveNameInputValue(event)} required/><br/>
-            <label for="dataUpload">Please upload your data</label><br/>
-            <input type="file" accept=".csv" onChange={(event) => {this.onFileChange(event, this.state.nameInputValue)}} required/>
-          </form>
+            <div className="formContainer">
+              <div>
+              <h1>Welcome</h1>
+              <p>Please complete your details, and upload<br/> your data to the platform.</p>
+              <form id="landingPageForm">
+                <label htmlFor="name">What is your name?</label>
+                <input type="text" name="name" id="name" value={this.state.nameInputValue} onChange={event => this.saveNameInputValue(event)} required/><br/>
+                <label htmlFor="dataUpload">Please upload your data</label><br/>
+                <input type="file" accept=".csv" onChange={(event) => {this.onFileChange(event, this.state.nameInputValue)}} required/>
+              </form>
+              </div>
+              <img src="images/workout.jpg" alt="2 people working out" weight="300" height="400"/>
+            </div>
+            <div>
           </div>
-          <img src="images/workout.jpg" alt="2 people working out" weight="300" height="400"/>
         </div>
-        <div>
-        
-      </div>
-    </div>
-    )
+        )
+      };
+
+      return (
+        <div className="popUpContainer">
+          <img src="images/running.png" alt="" weight="80" height="100"/>
+          <h1 id="uploadCompleteTxt">Upload Complete</h1>
+          <button id="pdfDownloadBtn"><img src="images/download.png" alt="" weight="200" height="100" onClick={this.savePDFDetails}/></button>
+          <p id="downloadPdfTxt">Download PDF</p>
+        </div>
+      )
+
+
+
+      // return (
+      //   <div>
+      //     <div className="formContainer">
+      //       <div>
+      //       <h1>Welcome</h1>
+      //       <p>Please complete your details, and upload<br/> your data to the platform.</p>
+      //       <form id="landingPageForm">
+      //         <label htmlFor="name">What is your name?</label>
+      //         <input type="text" name="name" id="name" value={this.state.nameInputValue} onChange={event => this.saveNameInputValue(event)} required/><br/>
+      //         <label htmlFor="dataUpload">Please upload your data</label><br/>
+      //         <input type="file" accept=".csv" onChange={(event) => {this.onFileChange(event, this.state.nameInputValue)}} required/>
+      //       </form>
+      //       </div>
+      //       <img src="images/workout.jpg" alt="2 people working out" weight="300" height="400"/>
+      //     </div>
+      //     <div>
+      //   </div>
+      // </div>
+      // )
   };
 };
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showPopUp: false};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState({
+      showPopUp: !this.state.showPopUp,
+    });
+  }
+
   render() {
     return (
-      <div class="landingPageContainer">
+      <div className="landingPageContainer">
         <div id="uploadDataContainer">
           <p id="uploadDataTxt">Upload data</p>
         </div>
-        <div class="pageForm">
-          <Form />
+        <div className="pageForm">
+          <Form showPopUp={this.state.showPopUp}/>
+          <button onClick={this.handleToggleClick}>
+          {this.state.showPopUp ? 'Back' : 'Upload File'}
+        </button>
         </div>
       </div>
     )
@@ -115,7 +202,7 @@ class LandingPage extends React.Component {
 class PopUp extends React.Component {
   render() {
     return (
-      <div class="popUpContainer">
+      <div className="popUpContainer">
         <img src="images/running.png" alt="" weight="80" height="100"/>
         <h1 id="uploadCompleteTxt">Upload Complete</h1>
         <button id="pdfDownloadBtn"><img src="images/download.png" alt="" weight="200" height="100"/></button>
